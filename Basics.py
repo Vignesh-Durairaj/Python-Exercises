b3f4991d5ecbb684cf38eb2bf20c1a2772074e06
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2018-12-06 12:23:01 +0530

    Head check-in for the python basic learnings.

---
+++ Basics.py
@@ -0,0 +1,16 @@
+# A sample comment line. Below is my very first code in python
+print ("Hello World !")
+print ("Welcome to the world of python")
+print ("Whatever you type within the 'print' function, it will be shown in the console")
+print ("And by the way.... remember SPAM and EGGS");
+
+# Let's do some mathematical operation and pring them in the console.
+print ("\n")
+print (12 / 7)
+print ("Adding 2 and 7 : " + str(2 + 7))
+print ("Subtracting 2 and 7 : " + str(2 - 7))
+print ("Multiuplying 2 and 7.0 :"  + str (2 * 7.0))
+print ("Dividing 2 and 7.0 :"  + str (2 / 7.0))
+print ("Remainder while dividing 1.25 with 0.5 : " + str (1.25 % 0.5))
+print ("Quotient while dividing 1.25 with 0.5 : " + str (1.25 // 0.5))
+print ("2 to the power of 7 : " + str(2 ** 7))
